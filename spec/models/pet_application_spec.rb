5f701632a45e03de0b2908b9c39272aa112fa43d
Gvieve <gnuebel84@yahoo.com>
2021-02-13 22:54:11 -0700

    Add test and code for pet has_many pet_applications

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe PetApplication, type: :model do
     it {should belong_to :application}
     it {should belong_to :pet}
   end
+  #
+  # it 'can have nil status' do
+  #   @application.update!(status: :pending)
+  #   expect(@application.status).to eq('pending')
+  #   expect(@application.pending?).to eq(true)
+  # end
+  #
+  # it 'can have approved status' do
+  #   @application.update!(status: :pending)
+  #   expect(@application.status).to eq('pending')
+  #   expect(@application.pending?).to eq(true)
+  # end
+  #
+  # it 'can have rejected status' do
+  #   @application.update!(status: :accepted)
+  #   expect(@application.status).to eq('accepted')
+  #   expect(@application.accepted?).to eq(true)
+  # end
 end
