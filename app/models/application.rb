1945f9b8b540bd2447031a5510e0922113b0ea94
Gvieve <gnuebel84@yahoo.com>
2021-02-13 22:44:07 -0700

    Update test and code application has_many pet_applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
+  has_many :pet_applications
+
   validates_presence_of :first_name, :last_name, :address, :city, :state, :zip, :adoption_reason
 
   enum status: [:in_progress, :pending, :accepted, :rejected]
