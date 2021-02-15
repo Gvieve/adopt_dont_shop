0b82db3a2894219efc143cec6e62c3091bd1592a
Gvieve <gnuebel84@yahoo.com>
2021-02-15 11:48:43 -0700

    Create relationship to pet/applcations through pet_applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,8 @@
 class Application < ApplicationRecord
   has_many :pet_applications
+  has_many :pets, through: :pet_applications
 
   validates_presence_of :first_name, :last_name, :address, :city, :state, :zip, :adoption_reason
 
-  enum status: [:in_progress, :pending, :accepted, :rejected]
+  enum status: [:in_progress, :pending, :approved, :rejected]
 end
