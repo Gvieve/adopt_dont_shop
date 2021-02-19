29abf345a20d35459fab20323f06b84c1162e1bf
Gvieve <gnuebel84@yahoo.com>
2021-02-18 20:42:23 -0700

    Create route, action, and view for admin/shelters#index

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,4 +2,13 @@ class AdminsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def shelters_index
+    @shelters = Shelter.sorted_by_name_desc
+    @shelters_with_pending_apps = Shelter.with_pending_apps
+  end
+
+  def shelters_show
+    @shelter = Shelter.find(params[:id])
+  end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,21 @@
 class Shelter < ApplicationRecord
   has_many :pets
+
+  def self.sorted_by_name_desc
+    find_by_sql("select * from shelters order by name desc")
+  end
+
+  def self.get_name_and_address(shelter_id)
+    find_by_sql(
+      "select name, address, city, state, zip from shelters where id = #{shelter_id}"
+    ).first.full_address
+  end
+
+  def full_address
+   "#{street_address ||= address} #{city} #{state} #{zip_code ||= zip}"
+  end
+
+  def self.with_pending_apps
+    Shelter.joins(pets: :applications).where(applications: {status: 1}).select(:name).distinct.order(:name)
+  end
 end
